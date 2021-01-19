03c51edaca3e5f70d2e66c60c7996eba1281202a
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-01-19 11:09:06 +0900

    商品情報編集機能・指摘取込２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :redirect_item, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,17 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params_update)
-      #   数字を送ってるだけ
       redirect_to item_path(@item.id)
     else
       render :edit
@@ -51,6 +48,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_item
     redirect_to action: :index unless current_user == @item.user
   end
 end
